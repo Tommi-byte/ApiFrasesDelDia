e124b68f61673c49e4480b54738e9ec2f14ed0be
Tom <cri.jimenez21@gmail.com>
2024-01-22 19:14:33 -0300

    se incluye biblioteca mysql2

--- server.js
+++ server.js
@@ -1,22 +1,103 @@
-const express = require('express');
-const mysql = require('mysql');
+const mysql  = require('mysql2/promise');
 const myconn = require('express-myconnection');
 const cors = require('cors');
+const express = require('express');
+const { createPool } = require('mysql2');
+const routes = express.Router();
 
-const routes = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 9000)
-const dboptions = {
-    host: 'aws.connect.psdb.cloud',
-    port: 3306,
-    user: '7dabj8xmed6ocd5olppr',
-    password: 'pscale_pw_OuKd3ETQM3hrpAuMYMYlk30Zlkw0EmT8Iv6rHn8upNw',
-    database: 'mysqltest'
+
+async function main() {
+    const conn =  await createPool ({
+        host: 'aws.connect.psdb.cloud',
+        user: '7dabj8xmed6ocd5olppr',
+        password: 'pscale_pw_OuKd3ETQM3hrpAuMYMYlk30Zlkw0EmT8Iv6rHn8upNw',
+        database: 'mysqltest',
+        ssl: {
+            rejectUnauthorized : false,
+        }
+    });
+
+    try {
+        // Crear una conexión a la base de datos
+
+        // Realizar operaciones con la base de datos aquí
+        routes.get('/obtener-frases', (req, res) => {
+            req.getConnection((err, conn) => {
+                if (err) return res.send(err);
+                conn.query('SELECT * FROM frases;', (error, rows) => {
+                    if (err) return res.send(err);
+                    res.json(rows);
+                });
+            })
+        })
+
+        routes.get('/obtener-frase/:id', (req, res) => {
+            req.getConnection((err, conn) => {
+                if (err) return res.send(err);
+                conn.query('SELECT * FROM frases WHERE id = ?;', [req.params.id], (error, rows) => {
+                    if (err) return res.send(err);
+                    res.json(rows);
+                });
+            })
+        })
+
+        routes.post('/agregar-frases', (req, res) => {
+            req.getConnection((err, conn) => {
+                if (err) return res.send(err);
+                console.log(req.body);
+                conn.query('INSERT INTO frases set ?', [req.body], (err, rows) => {
+                    if (err) return res.send(err);
+                    res.send('Frase Registrada con exito');
+                });
+            })
+        })
+
+        routes.put('/actualizar-frases/:id', (req, res) => {
+            req.getConnection((err, conn) => {
+                if (err) return res.send(err);
+                console.log(req.body);
+                conn.query('UPDATE  frases set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+                    if (err) return res.send(err);
+                    res.send('Frase Actualizado con exito');
+                });
+            })
+        })
+
+        routes.delete('/eliminar-frases/:id', (req, res) => {
+            req.getConnection((err, conn) => {
+                if (err) return res.send(err);
+                console.log(req.body);
+                conn.query('DELETE FROM frases WHERE id = ?', [req.params.id], (err, rows) => {
+                    if (err) return res.send(err);
+                    res.send('Frase Eliminada con exito');
+                });
+            })
+        })
+
+
+
+        module.exports = routes;
+
+        // Cerrar la conexión cuando hayas terminado
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error.message);
+    }
 }
 
-// ------------- MIDDLEWARES -------------------------- //
-app.use(myconn(mysql, dboptions, 'single'));
+main();
+
+// const dboptions = {
+//     host: 'aws.connect.psdb.cloud',
+//     port: 3306,
+//     user: '7dabj8xmed6ocd5olppr',
+//     password: 'pscale_pw_OuKd3ETQM3hrpAuMYMYlk30Zlkw0EmT8Iv6rHn8upNw',
+//     database: 'mysqltest'
+// }
+
+//------------- MIDDLEWARES -------------------------- //
 app.use(express.json());
 app.use(cors());
 // Configurar CORS para permitir solicitudes desde http://localhost:8100
