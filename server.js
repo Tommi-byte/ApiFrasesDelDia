bbf61645ec7ae633e2b70319fcff46d050d7ef12
Tom <cri.jimenez21@gmail.com>
2024-01-22 19:17:02 -0300

    cambios de sintaxis

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql  = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 const myconn = require('express-myconnection');
 const cors = require('cors');
 const express = require('express');
@@ -10,13 +10,13 @@ const app = express();
 app.set('port', process.env.PORT || 9000)
 
 async function main() {
-    const conn =  await createPool ({
+    const conn = await createPool({
         host: 'aws.connect.psdb.cloud',
         user: '7dabj8xmed6ocd5olppr',
         password: 'pscale_pw_OuKd3ETQM3hrpAuMYMYlk30Zlkw0EmT8Iv6rHn8upNw',
         database: 'mysqltest',
         ssl: {
-            rejectUnauthorized : false,
+            rejectUnauthorized: false,
         }
     });
 
@@ -77,43 +77,46 @@ async function main() {
             })
         })
 
+        //------------- MIDDLEWARES -------------------------- //
+        app.use(express.json());
+        app.use(cors());
+        // Configurar CORS para permitir solicitudes desde http://localhost:8100
+        app.use(cors({
+            origin: '*',
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            credentials: true,
+        }));
+        // ------------- ROUTES -------------------------- //
+        app.get('/', (req, res) => {
+            res.send('Welcome to my API');
+        })
+
+        app.use('/api', routes);
+
+        // ------------- Server Running -------------------------- //
+        app.listen(app.get('port'), () => {
+            co
 
 
-        module.exports = routes;
 
-        // Cerrar la conexión cuando hayas terminado
-    } catch (error) {
-        console.error('Error al conectar a la base de datos:', error.message);
+            module.exports = routes;
+            console.log('exitosa conexion');
+
+            // Cerrar la conexión cuando hayas terminado
+        } catch (error) {
+            console.error('Error al conectar a la base de datos:', error.message);
+        }
     }
-}
 
 main();
 
-// const dboptions = {
-//     host: 'aws.connect.psdb.cloud',
-//     port: 3306,
-//     user: '7dabj8xmed6ocd5olppr',
-//     password: 'pscale_pw_OuKd3ETQM3hrpAuMYMYlk30Zlkw0EmT8Iv6rHn8upNw',
-//     database: 'mysqltest'
-// }
+    // const dboptions = {
+    //     host: 'aws.connect.psdb.cloud',
+    //     port: 3306,
+    //     user: '7dabj8xmed6ocd5olppr',
+    //     password: 'pscale_pw_OuKd3ETQM3hrpAuMYMYlk30Zlkw0EmT8Iv6rHn8upNw',
+    //     database: 'mysqltest'
+    // }
 
-//------------- MIDDLEWARES -------------------------- //
-app.use(express.json());
-app.use(cors());
-// Configurar CORS para permitir solicitudes desde http://localhost:8100
-app.use(cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}));
-// ------------- ROUTES -------------------------- //
-app.get('/', (req, res) => {
-    res.send('Welcome to my API');
-})
-
-app.use('/api', routes);
-
-// ------------- Server Running -------------------------- //
-app.listen(app.get('port'), () => {
-    console.log('El servidor esta corriendo por el puerto', app.get('port'));
+    nsole.log('El servidor esta corriendo por el puerto', app.get('port'));
 });
