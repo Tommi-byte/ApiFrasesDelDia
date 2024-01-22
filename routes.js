aea3c363715e38c190522c6f14fc07e9f01f9359
Tom <cri.jimenez21@gmail.com>
2024-01-22 19:21:29 -0300

    nuevos cambios en el router

--- routes.js
+++ routes.js
@@ -1,59 +1,60 @@
 const express = require('express');
 const routes = express.Router();
 
+// Crear una conexión a la base de datos
+
+// Realizar operaciones con la base de datos aquí
 routes.get('/obtener-frases', (req, res) => {
-    req.getConnection( (err, conn) => {
-        if(err) return res.send(err);
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
         conn.query('SELECT * FROM frases;', (error, rows) => {
-            if(err) return res.send(err);
+            if (err) return res.send(err);
             res.json(rows);
         });
-    } )
+    })
 })
 
 routes.get('/obtener-frase/:id', (req, res) => {
-    req.getConnection( (err, conn) => {
-        if(err) return res.send(err);
-        conn.query('SELECT * FROM frases WHERE id = ?;', [req.params.id] , (error, rows) => {
-            if(err) return res.send(err);
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('SELECT * FROM frases WHERE id = ?;', [req.params.id], (error, rows) => {
+            if (err) return res.send(err);
             res.json(rows);
         });
-    } )
+    })
 })
 
 routes.post('/agregar-frases', (req, res) => {
-    req.getConnection( (err, conn) => {
-        if(err) return res.send(err);
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
         console.log(req.body);
-        conn.query('INSERT INTO frases set ?', [req.body] ,(err, rows) => {
-            if(err) return res.send(err);
+        conn.query('INSERT INTO frases set ?', [req.body], (err, rows) => {
+            if (err) return res.send(err);
             res.send('Frase Registrada con exito');
         });
-    } )
+    })
 })
 
 routes.put('/actualizar-frases/:id', (req, res) => {
-    req.getConnection( (err, conn) => {
-        if(err) return res.send(err);
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
         console.log(req.body);
-        conn.query('UPDATE  frases set ? WHERE id = ?', [req.body,req.params.id] ,(err, rows) => {
-            if(err) return res.send(err);
+        conn.query('UPDATE  frases set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err);
             res.send('Frase Actualizado con exito');
         });
-    } )
+    })
 })
 
 routes.delete('/eliminar-frases/:id', (req, res) => {
-    req.getConnection( (err, conn) => {
-        if(err) return res.send(err);
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
         console.log(req.body);
-        conn.query('DELETE FROM frases WHERE id = ?', [req.params.id] ,(err, rows) => {
-            if(err) return res.send(err);
+        conn.query('DELETE FROM frases WHERE id = ?', [req.params.id], (err, rows) => {
+            if (err) return res.send(err);
             res.send('Frase Eliminada con exito');
         });
-    } )
+    })
 })
-
-
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const mysql = require('mysql2/promise');
-const myconn = require('express-myconnection');
 const cors = require('cors');
 const express = require('express');
-const { createPool } = require('mysql2');
+const { createPool } = require('mysql2/promise');
 const routes = express.Router();
 
 
@@ -21,61 +19,6 @@ async function main() {
     });
 
     try {
-        // Crear una conexión a la base de datos
-
-        // Realizar operaciones con la base de datos aquí
-        routes.get('/obtener-frases', (req, res) => {
-            req.getConnection((err, conn) => {
-                if (err) return res.send(err);
-                conn.query('SELECT * FROM frases;', (error, rows) => {
-                    if (err) return res.send(err);
-                    res.json(rows);
-                });
-            })
-        })
-
-        routes.get('/obtener-frase/:id', (req, res) => {
-            req.getConnection((err, conn) => {
-                if (err) return res.send(err);
-                conn.query('SELECT * FROM frases WHERE id = ?;', [req.params.id], (error, rows) => {
-                    if (err) return res.send(err);
-                    res.json(rows);
-                });
-            })
-        })
-
-        routes.post('/agregar-frases', (req, res) => {
-            req.getConnection((err, conn) => {
-                if (err) return res.send(err);
-                console.log(req.body);
-                conn.query('INSERT INTO frases set ?', [req.body], (err, rows) => {
-                    if (err) return res.send(err);
-                    res.send('Frase Registrada con exito');
-                });
-            })
-        })
-
-        routes.put('/actualizar-frases/:id', (req, res) => {
-            req.getConnection((err, conn) => {
-                if (err) return res.send(err);
-                console.log(req.body);
-                conn.query('UPDATE  frases set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
-                    if (err) return res.send(err);
-                    res.send('Frase Actualizado con exito');
-                });
-            })
-        })
-
-        routes.delete('/eliminar-frases/:id', (req, res) => {
-            req.getConnection((err, conn) => {
-                if (err) return res.send(err);
-                console.log(req.body);
-                conn.query('DELETE FROM frases WHERE id = ?', [req.params.id], (err, rows) => {
-                    if (err) return res.send(err);
-                    res.send('Frase Eliminada con exito');
-                });
-            })
-        })
 
         //------------- MIDDLEWARES -------------------------- //
         app.use(express.json());
@@ -97,8 +40,6 @@ async function main() {
         app.listen(app.get('port'), () => {
             console.log('El servidor esta corriendo por el puerto', app.get('port'));
         });
-
-
 
         module.exports = routes;
         console.log('exitosa conexion');
